71408c7cc27fe485ad06249db356aa4bfff8e062
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-02-24 23:49:45 +0530

    Update Constructor_Execution.java

--- Inheritance/Constructor_Execution.java
+++ Inheritance/Constructor_Execution.java
@@ -1,9 +1,9 @@
 
-class Demo1
+class Demo1    // it's parent class is object class
 {
     int a, b;
     
-    public Demo1()
+    public Demo1()   
     {
         System.out.println("Parent class Constructor");
     }
@@ -19,24 +19,24 @@ class Demo2 extends Demo1
     int m,n;
     public Demo2()
     {
-        this(10,20);
+        this(10,20);     // it calls constructor within the same class
         System.out.println("Child class Constructor");
     }
 
     public Demo2(int x, int y)
     {
-        //super();
+        //super();      // will be used by default also.  It calls the parent class constructor
         System.out.println("parameterized child class Constructor");
         m=x;
         n=y;
     }
 }
-public class ConstructorExe 
+public class Constructor_Execution
 {
     public static void main(String[] args) 
     {
         
-        Demo2 d=new Demo2();
+        Demo2 d=new Demo2();     // it will call zero parametrized constructor
 
         //Demo2 d2=new Demo2(10,20);
     }
