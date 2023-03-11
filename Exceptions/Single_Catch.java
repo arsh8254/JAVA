7146c7fdf88b6ace85793776ef1a9fad9b613646
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-03-11 23:01:39 +0530

    1

---
+++ Exceptions/Single_Catch.java
@@ -0,0 +1,23 @@
+public class Single_Catch
+	{
+	public static void main(String[] args)
+	{
+		int num1 = 6;
+		int num2 = 0;
+		int result = 0;
+
+		try
+		{
+			result = num1/num2;
+			System.out.println("in try block");
+		}
+
+		catch(Exception obj)  // obj stores the exception value which can be printed
+		{
+			System.out.println("Something went wrong" + obj);
+		}
+        
+        System.out.println(result);
+		System.out.println("Bye");
+	}
+}
