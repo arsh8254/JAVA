88506776da3eebbae8c8da48d29eb20e40a40859
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-04-30 01:10:46 +0530

    1

---
+++ Collections/ArrayDeque1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ArrayDeque1
+{
+    public static void main(String[] args) 
+    {
+        ArrayDeque ad1=new ArrayDeque();
+
+        ad1.add(100);
+        ad1.add(200);
+        ad1.add(200);
+        System.out.println(ad1);
+
+        ad1.addFirst(10);
+        ad1.addLast(20);
+
+        System.out.println(ad1);
+        ad1.add("PW");
+        System.out.println(ad1);
+
+
+        ad1.offer(500);     //this data may or may not be added --for less important data
+        ad1.offerLast(10);
+        ad1.offerFirst(1);
+
+        System.out.println(ad1);
+        
+    }
+    
+}
