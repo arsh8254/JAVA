298a847f1ceb4b29bc5b6555e295840a477db97e
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-04-30 15:03:02 +0530

    1

--- Collections/TreeSet1.java
+++ Collections/TreeSet1.java
@@ -15,15 +15,15 @@ public class TreeSet1
        ts.add(175);
 
        System.out.println(ts);
-       
+
        // ts.add(100);
        //System.out.println(ts);
 
-       System.out.println(ts.higher(50));
-       System.out.println(ts.lower(50));
+       System.out.println(ts.higher(50));   //gives one value higher than 50
+       System.out.println(ts.lower(50));    //gives one value lower than 50
 
-       System.out.println(ts.ceiling(40));
-       System.out.println(ts.floor(40));
+       System.out.println(ts.ceiling(40));  //if 40 is not present then it  will give higher value or 40 only if 40 is present in collection
+       System.out.println(ts.floor(40));    // same as above but lower value
 
        System.out.println(ts.ceiling(50));
        System.out.println(ts.floor(50));
