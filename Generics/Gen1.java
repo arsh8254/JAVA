b362ad00ddbdd6455666ff9fbe19266c6b762f3d
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-05-02 17:42:54 +0530

    1

--- Generics/Gen1.java
+++ Generics/Gen1.java
@@ -4,29 +4,29 @@ public class Gen1
 {
     public static void main(String[] args) 
     {
-        //typesafety
+        //typesafety--data type must always be same
         // String ar[]=new String[10];
         // ar[0]="Hyder";
         // ar[1]="PW";
-        // //ar[2]=10; CE
+        // //ar[2]=10; Compile time error
 
         // String name1=ar[0];
         // String name2=ar[1];
 
-        //No typsesafety
-        //Typesafety achived usimg generics
+        
+        //Typesafety is achieved usimg generics
         ArrayList<String> al=new ArrayList<String>();
 
         al.add("Hyder");
         al.add("PW");
 
-        //al.add(10);
+        //al.add(10);   //compile time error
 
         // String n1=(String)al.get(0); //typecasting
         // String n2=(String)al.get(1);
         // String n3=(String)al.get(2);
 
-        String name1 =al.get(0);
+        String name1 =al.get(0);   // no need of typecating now after using generics
         String name2 =al.get(1);
 
 
