f9bdab1b087a31e2e7ccd12c8771dc50b876c819
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-05-15 05:29:52 +0530

    1

--- APIs/Enums1.java
+++ APIs/Enums1.java
@@ -1,16 +1,13 @@
 
-//enum -- 1.5 
-
 enum Week
 {
     MON, TUE, WED, THU, FRI, SAT, SUN;
 }
 
-public class LaunchEnum1 
+public class Enums1 
 {
 
-   
-    enum Result
+    enum Result         // can be defined within or outside the class
     {
         PASS, FAIL, NR;
     }
@@ -18,18 +15,16 @@ public class LaunchEnum1
     public static void main(String[] args) 
     {
 
+        Week we=Week.MON;
+        System.out.println(we);
 
-        Week w=Week.MON;
-        System.out.println(w);
-
-        int index =Week.MON.ordinal();
+        int index =Week.MON.ordinal();   // ordinal method gives the index of element
         System.out.println(index);
 
-       Week []wr=Week.values();
-       for(Week w1: wr)
+       Week[] wr=Week.values();
+       for(Week w: wr)
        {
-        System.out.println(w1 + " : " + w1.ordinal());
-
+        System.out.println(w + " : " + w.ordinal());
        }
 
         // Result r=Result.PASS;
