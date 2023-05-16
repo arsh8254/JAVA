5acf44095e56d657d56369c30c03f11f4934fc73
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-05-17 00:17:56 +0530

    1

---
+++ IO_Operation/File1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class File1 
+{
+    public static void main(String[] args) throws IOException
+    {
+        
+        File file1=new File("pw.txt");
+
+        System.out.println(file1.exists());   //false
+
+        file1.createNewFile();
+        System.out.println(file1.exists());   // true
+
+
+        File dir=new File("PW");
+        System.out.println(dir.exists());    //false
+
+        dir.mkdir();
+
+        System.out.println(dir.exists());   //true
+        
+    }
+    
+}
