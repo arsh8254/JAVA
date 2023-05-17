be2a8d390462aabd0c06dba57292a425f5ced76f
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-05-17 16:55:58 +0530

    1

--- IO_Operation/File2.java
+++ IO_Operation/File2.java
@@ -5,13 +5,14 @@ public class File2
     public static void main(String[] args)  throws IOException
     {
         File dir=new File("PWJAVA");
-     // System.out.println(dir.isDirectory());
+        // System.out.println(dir.isDirectory());   // it also checks if directory exists or not
+
         dir.mkdir();
-        System.out.println("dir is refering to directory PWJAVA :" + dir.isDirectory());
+        System.out.println("dir is referring to directory PWJAVA :" + dir.isDirectory());
 
         File file=new File(dir, "pwskill.txt");
         file.createNewFile();
-        System.out.println("file is refering to pwskill.txt : " + file.isFile());
+        System.out.println("file is referring to pwskill.txt : " + file.isFile());
 
             int count =0;
 
@@ -20,7 +21,7 @@ public class File2
 
             String str[]=f.list();
 
-            for(String name: str)
+            for(String name: str)    // to iterate and print each file name
             {
                 count++;
                 System.out.println(name);
