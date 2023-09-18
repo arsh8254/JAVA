f495c640ddd2fa55fdd98a3982b1c9d55e65ea07
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-09-18 23:55:23 +0530

    1

---
+++ DSA/BackTracking/All_Sequences.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class All_Sequences {
+    public static void printSequences(int[] arr, int index, ArrayList<Integer> tempArr)
+    {
+        //base case condition
+        if(index == arr.length) {
+            //print all the sequences -- but do not print empty entries
+            if (tempArr.size() > 0) {
+                System.out.println(tempArr);
+            }
+            return;  // since it is arraylist, we must break out of loop or it will run forever as size will keep increasing always
+        }
+
+        //recursive call
+        //include
+        printSequences(arr, index+1, tempArr);
+        //add the value in the tempArray
+        tempArr.add(arr[index]);
+
+        //do not include
+        printSequences(arr, index+1, tempArr);
+        //remove the value from tempArray -- backtracking
+        tempArr.remove(tempArr.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2};
+        System.out.println("For the array - " + Arrays.toString(arr1));
+        printSequences(arr1, 0, new ArrayList<Integer>());
+        System.out.println();
+
+        int[] arr2 = {1, 2, 3};
+        System.out.println("For the array - " + Arrays.toString(arr2));
+        printSequences(arr2, 0, new ArrayList<Integer>());
+    }
+}
