0525415bce0e27d41d2dcf9b758192fbf41ec95a
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-09-25 16:05:01 +0530

    1

--- DSA/LinkedLists/Deletion.java
+++ DSA/LinkedLists/Deletion.java
@@ -11,55 +11,14 @@ class LinkedList {
         }
     }
 
-
-    // implementation of insertion of a node at the end
-    public void insertAtEnd(int newData){
-        Node newNode = new Node(newData);
-
-        // linkedlist is empty
-        if(head == null){
-            head = new Node(newData);
-            return;
-        }
-
-        // linkedlist is not empty
-        newNode.next = null;
-        Node temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-
-        temp.next = newNode;
-        return;
-    }
-
-    //  implementation of insertion of a node at the beginning
-    public void insertAtBeginning(int newData){
-        Node newNode = new Node(newData);
-        newNode.next = head;
-        head = newNode;
-    }
-
-    // implementation of insertion of a node after any node
-    public void insertAfter(Node prev_node, int newData){
-        if(prev_node == null){
-            System.out.println("The previous node cannot contain the null values");
-            return;
-        }
-        Node newNode = new Node(newData);
-        newNode.next = prev_node.next;
-        prev_node.next = newNode;
-
-    }
-
-      // implementation of deletion of a node in the linkedlist
+    }// deletion of a node in the linkedlist
       void deleteNode(int position){
         // linkedlist is empty
         if(head == null){
             return;
         }
-
         Node temp = head;
+
         // deletion from the beginning
         if(position == 0){
             head = temp.next;
@@ -74,7 +33,6 @@ class LinkedList {
         if(temp == null && temp.next == null){
             return;
         }
-
         temp.next = temp.next.next;
     }
     
@@ -102,7 +60,6 @@ class LinkedList {
         llist.insertAtBeginning(1);
         llist.insertAtBeginning(19);
 
-    
         System.out.println("After insertion of 10, 1 and 19");
         //llist.displayLL();
         System.out.println();
@@ -112,8 +69,7 @@ class LinkedList {
         System.out.println();
 
         llist.deleteNode(3);
-        System.out.println("Linked List after deletion of the node:");
+        System.out.println("Linked List after deletion of the node: ");
         llist.displayLL();
         System.out.println();
     }
-}
